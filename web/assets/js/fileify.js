ab796c468e4dc160aceba6d461669819bf425980
ShadowElf37 <ykey-cohen@emeryweiner.org>
2019-04-07 19:17:49 -0500

    ALPHA v0.0.2a (promise-free fileify)

--- web/assets/js/fileify.js
+++ web/assets/js/fileify.js
@@ -1,24 +1,32 @@
 // Courtesy of Alwinfy
 
 const fileify = form => {
-        const die = str => {throw new Error(str)};
-        if(!form instanceof HTMLFormElement)
-          die("pls fileify a form");
-        form.addEventListener("submit", function(ev) {
-          Promise.all([].map.call(form.querySelectorAll("input[type=file]"),
-              async function(inp) {
-            if(!inp.name) die("file input need name");
-            const file = inp.files[0] || die("No file selected");
-            const filedata = document.createElement("input");
-            filedata.type = "hidden";
-            filedata.name = "_" + inp.name;
-            filedata.value = escape(await (new Response(file)).text());
-            form.appendChild(filedata);
-          })).then(() => form.submit());
-          ev.preventDefault();
-        });
-      };
+  const die = str => {throw new Error(str)};
+  if(!form instanceof HTMLFormElement)
+    die("pls fileify a form");
+  form.addEventListener("submit", ev => {
+    const files = form.querySelectorAll("input[type=file]");
+    let count = files.length;
+    files.forEach(inp => {
+      if(!inp.name) die("file input need name");
+      const file = inp.files[0] || die("No file selected");
+      const filedata = document.createElement("input");
+      filedata.type = "hidden";
+      filedata.name = "_" + inp.name;
+      const reader = new FileReader();
+      reader.addEventListener("loadend", () => {
+        filedata.value = escape(reader.result);
+        form.appendChild(filedata);
+        if(!--count)
+          form.submit();
+      });
+      reader.readAsBinaryString(file);
+    });
+    ev.preventDefault();
+    return false;
+  });
+};
 
-      // client code
-      const form = document.querySelector('form'); // change to suit needs
-      fileify(form);
+// client code
+const form = document.querySelector('form'); // change to suit needs
+fileify(form);
