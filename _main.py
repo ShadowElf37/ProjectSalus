42b0d7e676c86e103928a2e5b87aef1b9ecd6c51
ShadowElf37 <ykey-cohen@emeryweiner.org>
2019-04-15 19:07:42 -0500

    ALPHA v0.0.10a (hotfix)

--- _main.py
+++ _main.py
@@ -3,7 +3,6 @@ import os.path as op
 import sys
 from tee import *
 from datetime import datetime
-from cache import FileCache
 
 # Obviously change these backslashes for Linux
 logfile = open("{here}\\logs\\{date}.log".format( \
