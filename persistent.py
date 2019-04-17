b836e8b3f34d47a50d9846aed1c8e171d5aa7ebf
Alwinfy <20421383+Alwinfy@users.noreply.github.com>
2019-04-16 20:10:30 -0500

    fix filename mishap

--- persistent.py
+++ persistent.py
@@ -3,9 +3,10 @@ import pickle
 
 config = get_config('persistent')
 
-class PersistentDict:
-    def __init__(self, name, default):
+class PersistentThing:
+    def __init__(self, name, default, eachwrite=False):
         self.name = name
+        self.eachwrite = eachwrite
         self.fname = "{}/{}.dat".format(config.get('directory'), name)
         try:
             self.fh = open(self.fname, "rb+")
@@ -21,11 +22,26 @@ class PersistentDict:
         return self.value   
     def set(self, value):
         self.value = value
-        self.update()
+        self.autowrite()
     def read(self):
         self.fh.seek(0)
-        self.value = pickle.load
+        self.value = pickle.load(self.fh)
+    def autowrite(self):
+        if eachwrite:
+            self.write()
     def write(self):
         self.fh.seek(0)
         self.fh.truncate()
         pickle.dump(self.value, self.fh)
+
+class PersistentDict(PersistentThing):
+    def __init__(self, name, eachwrite=False):
+        super().__init__(name, {}, eachwrite)
+    def get(self, key, default=None):
+        return super().get().get(key, default)
+    def set(self, key, value):
+        super().get()[key] = value
+        self.autowrite()
+    def delete(self, key):
+        del super().get()[key]
+        self.autowrite()
