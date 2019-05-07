f369aec4eb95a741906d83873b91150bc41f89e0
alqaadi <yovelkeycohen@gmail.com>
2019-05-07 11:49:48 -0500

    v0.1.4b (todo changes)

--- server/response.py
+++ server/response.py
@@ -95,8 +95,8 @@ class Response:
         # self.req.send_error(n, msg)
         # self.sent_prematurely = True
 
-    def redirect(self, location, permanent=False, get=False):
-        self.set_code(303 if get else 307 if not permanent else 308)
+    def redirect(self, location, permanent=False, force_get=False):
+        self.set_code(303 if force_get else 307 if not permanent else 308)
         self.add_header('Location', location)
 
     def load_base(self, content_type=None):
