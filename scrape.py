9ea5e46d2ad8ba26e90dc7afc7e0e8c59ec5786a
ShadowElf37 <ykey-cohen@emeryweiner.org>
2019-05-16 06:54:54 -0500

    ALPHA v0.2.0a (Selenium!)

--- scrape.py
+++ scrape.py
@@ -42,6 +42,40 @@ def log_error(e):
 
 def htmlobj(url):
     return BeautifulSoup(simple_get(url), 'html.parser')
+def html_fromtext(text):
+    return BeautifulSoup(text, 'html.parser')
 
 
-print(htmlobj('http://www.sagedining.com/menus/emeryweiner').find("div", {"id": "sageMbDailyViewFrame"}))
+# print(htmlobj('http://www.sagedining.com/menus/emeryweiner').find("div", {"id": "sageMbDailyViewFrame"}))
+
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+print('Starting.')
+driver = webdriver.Firefox()
+print('Fetching.')
+#driver.get("http://www.sagedining.com/menus/emeryweiner")
+#assert "SAGE" in driver.title
+#print('Waiting for element.')
+#elem = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "sageMbDailyViewFrame")))
+
+driver.get('https://emeryweiner.myschoolapp.com/app/student#login')
+user = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "Username")))
+user.send_keys('ykey-cohen')
+submit = driver.find_element_by_id('nextBtn')
+submit.click()
+pwd = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "Password")))
+pwd.send_keys('Yoproductions3')
+submit = driver.find_element_by_id('loginBtn')
+submit.click()
+WebDriverWait(driver, 5).until(EC.url_matches('https://emeryweiner.myschoolapp.com/app/student#activitystream'))
+driver.get('https://emeryweiner.myschoolapp.com/app/student#studentmyday/assignment-center')
+schedule = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "calendar-main-view")))
+print(html_fromtext(schedule.get_attribute('innerHTML')))
+# elem = driver.find_element_by_id("email")
+# elem = driver.find_element_by_id("pass")
+# elem.send_keys(Keys.RETURN)
+driver.close()
