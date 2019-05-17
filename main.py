59ca45ea57dbed32a3b617f563ad075318a1e9af
ShadowElf37 <ykey-cohen@emeryweiner.org>
2019-05-16 21:04:30 -0500

    ALPHA v0.2.2

--- main.py
+++ main.py
@@ -3,8 +3,18 @@ from subprocess import Popen
 import sys
 import os
 
+import importlib.util as iutil
+assert iutil.find_spec('selenium'), iutil.find_spec('bs4')
+print('Library check passed.')
+
 here = os.path.dirname(os.path.realpath(__file__))
 os.chdir(here)
+tomake = 'logs', 'data', 'config', 'web/assets'
+for folder in tomake:
+    try:
+        os.mkdir(folder)
+    except FileExistsError:
+        pass
 
 while True:
     process = Popen([sys.executable, '_main.py'],
@@ -14,7 +24,7 @@ while True:
     process.wait()
     if process.returncode == 37:
         process.kill()
-        print('REBOOTING')
+        print('Rebooting.')
         continue
     elif process.returncode == 0:
         print('Shut down gracefully.')
--- scrape.py
+++ scrape.py
@@ -8,6 +8,93 @@ from time import strftime
 from bs4 import BeautifulSoup
 import platform
 
+class Tab:
+    def __init__(self, driver, window):
+        self.window: str = window
+        self.browser: Browser = driver
+
+    @staticmethod
+    def focused(f):
+        def dec(self, *args, **kwargs):
+            old_tab = self.browser.current_tab()
+            if old_tab != self.window: self.focus()
+            r = f(self, *args, **kwargs)
+            if old_tab != self.window: self.browser.switch_tab_key(old_tab)
+            return r
+        return dec
+    focused = focused.__func__  # Necessary to avoid conflict between decorator and staticmethod
+
+    def focus(self):
+        return self.browser.switch_tab_key(self.window)
+    def get_tab(self):
+        return self.window
+    def page_html(self):
+        return Browser.soup(self.getElementByTag('html'))
+    def is_focused(self):
+        return self.browser.current_tab() == self.window
+
+    @focused
+    def wait_for(self, ecmethod, timeout=6):
+        self.browser.wait_for(ecmethod, timeout)
+    @focused
+    def wait_for_not(self, ecmethod, timeout=6):
+        self.browser.wait_for_not(ecmethod, timeout)
+
+    @focused
+    def open(self, url):
+        self.browser.open(url)
+    get = open
+
+    @focused
+    def get_title(self):
+        return self.browser.get_title()
+    @focused
+    def await_title_is(self, title, timeout=5):
+        return self.browser.await_title_is(title, timeout)
+    @focused
+    def get_url(self):
+        return self.browser.get_url()
+    @focused
+    def await_url_is(self, url, timeout=5):
+        return self.browser.await_url_is(url, timeout)
+
+    @focused
+    def run_js(self, js):
+        return self.browser.run_js(js)
+
+    @focused
+    def getElementById(self, id, timeout=2, multiple=False):
+        return self.browser.getElementById(id, timeout, multiple)
+    @focused
+    def getElementByClass(self, cls, timeout=2, multiple=False):
+        return self.browser.getElementById(cls, timeout, multiple)
+    @focused
+    def getElementByTag(self, tag, timeout=2, multiple=False):
+        return self.browser.getElementById(tag, timeout, multiple)
+    @focused
+    def getElementBySelector(self, css_sel, timeout=2, multiple=False):
+        return self.browser.getElementById(css_sel, timeout, multiple)
+    @focused
+    def findElement(self, by: By, value, timeout=2, multiple=False):
+        return self.browser.findElement(by, value, timeout, multiple)
+
+    def close(self):
+        self.browser.close_tab_key(self.window)
+    def duplicate(self):
+        self.browser._new_tab()
+        self.browser.get(self.get_url())
+    @focused
+    def refresh(self):
+        return self.browser.refresh()
+    @focused
+    def back(self):
+        return self.browser.back()
+    @focused
+    def forward(self):
+        return self.browser.forward()
+
+
+
 class Browser:
     OS = platform.system()
     CTRL = Keys.COMMAND if OS == 'Darwin' else Keys.CONTROL
@@ -16,16 +103,24 @@ class Browser:
         self.options = Options()
         self.options.headless = False
         self.driver = webdriver.Firefox(options=self.options)
+        self.tab_objects = []
 
     @staticmethod
-    def source(browser_obj):
+    def source(browser_obj) -> str:
         return browser_obj.get_attribute('innerHTML')
     @staticmethod
-    def soup(browser_obj):
+    def soup(browser_obj) -> BeautifulSoup:
         return BeautifulSoup(Browser.source(browser_obj), 'html.parser')
 
-    def open(self, url):
+    def open(self, url) -> Tab:
         self.driver.get(url)
+        if len(self.tab_objects) < 1:
+            t = Tab(self, self.current_tab())
+            self.tab_objects.append(t)
+        else:
+            t: Tab = self.get_tab_by_key(self.current_tab())
+        return t
+    get = open
 
     def wait_for(self, ecmethod, timeout=6):
         return WebDriverWait(self.driver, timeout).until(ecmethod)
@@ -35,8 +130,8 @@ class Browser:
     def getElementById(self, id, timeout=2, multiple=False):
         if timeout:
             if multiple:
-                return WebDriverWait(self.driver, timeout).until(EC.presence_of_all_elements_located((By.ID, id)))
-            return WebDriverWait(self.driver, timeout).until(EC.presence_of_element_located((By.ID, id)))
+                return self.wait_for(EC.presence_of_all_elements_located((By.ID, id)), timeout)
+            return self.wait_for(EC.presence_of_element_located((By.ID, id)), timeout)
         if multiple:
             self.driver.find_elements_by_id(id)
         return self.driver.find_element_by_id(id)
@@ -44,8 +139,8 @@ class Browser:
     def getElementByClass(self, cls, timeout=2, multiple=False):
         if timeout:
             if multiple:
-                return WebDriverWait(self.driver, timeout).until(EC.presence_of_all_elements_located((By.CLASS_NAME, cls)))
-            return WebDriverWait(self.driver, timeout).until(EC.presence_of_element_located((By.CLASS_NAME, cls)))
+                return self.wait_for(EC.presence_of_all_elements_located((By.CLASS_NAME, cls)), timeout)
+            return self.wait_for(EC.presence_of_element_located((By.CLASS_NAME, cls)), timeout)
         if multiple:
             self.driver.find_elements_by_class_name(cls)
         return self.driver.find_element_by_class_name(cls)
@@ -53,8 +148,8 @@ class Browser:
     def getElementBySelector(self, css_sel, timeout=2, multiple=False):
         if timeout:
             if multiple:
-                return WebDriverWait(self.driver, timeout).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, css_sel)))
-            return WebDriverWait(self.driver, timeout).until(EC.presence_of_element_located((By.CSS_SELECTOR, css_sel)))
+                return self.wait_for(EC.presence_of_all_elements_located((By.CSS_SELECTOR, css_sel)), timeout)
+            return self.wait_for(EC.presence_of_element_located((By.CSS_SELECTOR, css_sel)), timeout)
         if multiple:
             self.driver.find_elements_by_css_selector(css_sel)
         return self.driver.find_element_by_css_selector(css_sel)
@@ -62,43 +157,67 @@ class Browser:
     def getElementByTag(self, tag, timeout=2, multiple=False):
         if timeout:
             if multiple:
-                return WebDriverWait(self.driver, timeout).until(EC.presence_of_all_elements_located((By.TAG_NAME, tag)))
-            return WebDriverWait(self.driver, timeout).until(EC.presence_of_element_located((By.TAG_NAME, tag)))
+                return self.wait_for(EC.presence_of_all_elements_located((By.TAG_NAME, tag)), timeout)
+            return self.wait_for(EC.presence_of_element_located((By.TAG_NAME, tag)), timeout)
         if multiple:
             self.driver.find_elements_by_tag_name(tag)
         return self.driver.find_elements_by_tag_name(tag)
 
+    def findElement(self, by: By, value, timeout=2, multiple=False):
+        if timeout:
+            if multiple:
+                return self.wait_for(EC.presence_of_all_elements_located((by, value)), timeout)
+            return self.wait_for(EC.presence_of_element_located((by, value)), timeout)
+        if multiple:
+            return self.driver.find_elements(by, value)
+        return self.driver.find_element(by, value)
+
     def await_url_is(self, url, timeout=5):
-        return WebDriverWait(self.driver, timeout).until(EC.url_matches(url))
+        return self.wait_for(EC.url_matches(url), timeout)
     def get_url(self):
         return self.driver.current_url
 
     def await_title_is(self, title, timeout=5):
-        return WebDriverWait(self.driver, timeout).until(EC.title_is(title))
+        return self.wait_for(EC.title_is(title), timeout)
     def get_title(self):
         return self.driver.title
 
-    def get_tab(self, n):
+    def get_tab(self, n) -> Tab:
+        return self.tab_objects[n]
+    def get_tab_by_key(self, key) -> Tab:
+        try:
+            return next(filter(lambda t: t.window == key, self.tab_objects))
+        except StopIteration:
+            raise KeyError('Key does not match any tab objects.')
+    def _get_tab(self, n) -> str:
         return self.driver.window_handles[n]
-    def current_tab(self):
+    def current_tab(self) -> str:
         return self.driver.current_window_handle
-    def await_new_tab(self, timeout=5):
-        return WebDriverWait(self.driver, timeout).until(EC.new_window_is_opened(self.driver.window_handles))
-    def new_tab(self):
-        r = self.driver.execute_script("window.open('', '_blank')")
+    def await_new_tab(self, timeout=1):
+        return self.wait_for(EC.new_window_is_opened(self.driver.window_handles), timeout)
+    def _new_tab(self):
+        r = self.run_js("window.open('', '_blank')")
         self.switch_last_tab()
         return r
+    def new_tab(self) -> Tab:
+        self._new_tab()
+        t = Tab(self, self.current_tab())
+        self.tab_objects.append(t)
+        return t
     def switch_tab(self, n):
         try:
-            return self.driver.switch_to.window(self.get_tab(n))
+            return self.driver.switch_to.window(self._get_tab(n))
         except IndexError:
             return self.switch_last_tab()
-    def switch_tab_obj(self, tabobj):
-        return self.driver.switch_to.window(tabobj)
+    def switch_tab_key(self, tabkey):
+        return self.driver.switch_to.window(tabkey)
     def switch_last_tab(self):
         return self.switch_tab(-1)
+    def close_tab_key(self, key):
+        self.close_tab(self.driver.window_handles.index(key))
+        self.tab_objects.remove(self.get_tab_by_key(key))
     def close_tab(self, n=None):
-        current = self.current_tab() if n is None else self.get_tab(n)
+        current = self.current_tab() if n is None else self._get_tab(n)
         tabs = self.driver.window_handles.copy()
         current_index = tabs.index(current)
         tabs.remove(current)
@@ -110,19 +229,19 @@ class Browser:
             self.driver.window_handles.append(tab)
         self.switch_tab(current_index - 1)
 
-    def get_visible(self, obj):
+    def is_visible(self, obj):
         return EC.visibility_of(obj)
-    def get_invisible(self, obj):
+    def is_invisible(self, obj):
         return EC.invisibility_of_element(obj)
 
-    def do_js(self, js):
+    def run_js(self, js):
         return self.driver.execute_script(js)
 
     def set_cookies(self, **kwargs):
         self.driver.add_cookie(kwargs)
-    def get_cookie(self, k):
+    def get_cookie(self, k) -> str:
         return self.driver.get_cookie(k)
-    def get_cookies(self):
+    def get_cookies(self) -> list:
         return self.driver.get_cookies()
 
     def screenshot(self):
@@ -139,28 +258,25 @@ class Browser:
         return self.driver.close()
 
 
-test = Browser()
-test.open('https://emeryweiner.myschoolapp.com/app/student#login')
+firefox = Browser()
+blackbaud = firefox.open('https://emeryweiner.myschoolapp.com/app/student#login')
 
-user = test.getElementById('Username', 5)
-submit = test.getElementById('nextBtn')
+user = blackbaud.getElementById('Username', 5)
+submit = blackbaud.getElementById('nextBtn')
 user.send_keys('ykey-cohen')
 submit.click()
 
-pwd = test.getElementById('Password')
-pwd.send_keys('no password for you hehe')
-submit = test.getElementById('loginBtn')
+pwd = blackbaud.getElementById('Password')
+pwd.send_keys('Yoproductions3')
+submit = blackbaud.getElementById('loginBtn')
 submit.click()
-test.await_url_is('https://emeryweiner.myschoolapp.com/app/student#activitystream')
+blackbaud.await_url_is('https://emeryweiner.myschoolapp.com/app/student#activitystream')
 
-test.new_tab()
-test.open('https://twitter.com')
-test.switch_tab(0)
-test.open('https://emeryweiner.myschoolapp.com/app/student#studentmyday/assignment-center')
-test.switch_tab(1)
-test.open('https://google.com')
-test.close_tab()
+twitter = firefox.new_tab()
+twitter.open('https://twitter.com')
+blackbaud.open('https://emeryweiner.myschoolapp.com/app/student#studentmyday/assignment-center')
+twitter.close()
 
-schedule = test.getElementById('calendar-main-view', 5)
-print(test.soup(schedule))
-test.close()
+schedule = blackbaud.getElementById('calendar-main-view', 5)
+print(firefox.soup(schedule))
+firefox.close()
