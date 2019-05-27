eacb837acd281592dda4625e6fd10f3c0d2126f2
Alwinfy <20421383+Alwinfy@users.noreply.github.com>
2019-05-27 15:25:46 -0500

    Do a big modularization overhaul of serializer

--- server/serializer.py
+++ server/serializer.py
@@ -1,3 +1,4 @@
+from enum       import Enum
 from threading  import RLock
 from uuid       import uuid4
 from functools  import wraps
@@ -15,11 +16,83 @@ class Dummy:
     pass
 def noop(*args, **kwargs):
     pass
+def noop2(self, arg):
+    return arg
 
+class Priority(Enum):
+    BEFORE  = lambda queue, item: queue.insert(0, item)
+    AFTER   = lambda queue, item: queue.append(item)
+class BaseSerializer:
+    _serializes = []
+    _deserializes = []
+    def _serialize(self, obj):
+        for (pred, func) in self.__class__._serializes:
+            if pred(self, obj):
+                return func(self, obj)
+        raise TypeError("Attempting to _serialize non-serializable thing {}".format(obj))
+    def _deserialize(self, obj):
+        for (pred, func) in self.__class__._deserializes:
+            if pred(self, obj):
+                return func(self, obj)
+        raise TypeError("Unsupported deserialization for {}!".format(obj))
+    @staticmethod
+    def wrap(type, data):
+        return {"type": type, "data": data}
+    @staticmethod
+    def is_wrapped(wrapped):
+        return type(wrapped) is dict and "type" in wrapped and "data" in wrapped
+def can_serialize(spredicate, serialize, dpredicate, deserialize, priority: Priority=Priority.AFTER):
+    def serializer_decor(cls):
+        def validate(thing):
+            if callable(thing): return thing
+            thing = getattr(cls, thing, None)
+            if callable(thing): return thing
+            raise TypeError("Not a callable or method of the current class!")
+        if not issubclass(cls, BaseSerializer):
+            raise TypeError("Cannot decorate a non-serializer!")
+        priority(cls._serializes,   (validate(spredicate), validate(serialize)))
+        priority(cls._deserializes, (validate(dpredicate), validate(deserialize)))
+        return cls
+    return serializer_decor
 
-class Serializer:
+@can_serialize(lambda val: type(val) is bytes, "_serialize_bytes", "_is_bytes", lambda val: bytes.fromhex(val["data"]))
+@can_serialize("_is_primitive", noop2, "_is_primitive", noop2)
+class PrimitiveSerializer(BaseSerializer):
     PRIMITIVE_TYPES = (str, int, float, bool, type(None))
+    def _is_primitive(self, obj):
+        """Checks primitivity-- direct serialization for these"""
+        return type(obj) in self.__class__.PRIMITIVE_TYPES
+    def _is_bytes(self, obj):
+        return self.is_wrapped(obj) and obj["type"] == "bytes"
+    def _serialize_bytes(self, obj):
+        return self.wrapped("bytes", obj.hex())
+
+@can_serialize("_is_siterable", "_serialize_iterable", "_is_diterable", "_deserialize_iterable")
+@can_serialize(lambda val: type(val) is dict, "_serialize_dict", "_is_dict", "_deserialize_dict")
+class RecursiveSerializer(PrimitiveSerializer):
     ITERABLE_TYPES  = (list, set, tuple)
+    def _is_siterable(self, obj):
+        return type(obj) in self.__class__.ITERABLE_TYPES
+    def _serialize_iterable(self, obj):
+        return {"type": type(obj).__name__, "data":
+            [self._serialize(i) for i in iterable]}
+    def _is_diterable(self, obj):
+        return self.is_wrapped(obj) and obj["type"] in map(lambda x: x.__name__, self.__class__.ITERABLE_TYPES)
+    def _deserialize_iterable(self, obj):
+        return __builtins__.get(obj["type"], None)(self._deserialize(i) for i in obj["data"])
+
+    def _serialize_dict(self, obj):
+        return {"type": "dict", "data":
+            [{"key": self._serialize(k), "value": self._serialize(v)}
+                for (k, v) in obj.items()]}
+    def _is_dict(self, obj):
+        return self.is_wrapped(obj) and obj["type"] == "dict"
+    def _deserialize_dict(self, obj):
+        return {x["key"]: self._deserialize(x["value"]) for x in obj["data"]}
+
+@can_serialize("_is_sclass", "_from_pool", "_is_dsclass", "_deserialize_class", Priority.BEFORE)
+class ClassSerializer(RecursiveSerializer):
+    RPF             = config.get("ref_prefix")
     def __init__(self):
         self.names = dict()
         self.antipool = dict() # map uuid to serialized data
@@ -27,9 +100,6 @@ class Serializer:
         self.pool = dict() # map uuid to name
         self.pool_queue = None
         self.pool_lock = RLock()
-
-    def __del__(self):
-        pass
 
     def load(self, file):
         """Read serialized data from a file."""
@@ -73,30 +143,10 @@ class Serializer:
             cls._serialize = lambda obj, ser: ser._serialize_class(obj, cls.__module__)
             return cls
         return s_decor
-
-    def _is_primitive(self, obj):
-        """Checks primitivity-- direct serialization for these"""
-        return type(obj) in Serializer.PRIMITIVE_TYPES
     
     def _is_sclass(self, thing):
-        """Make sure a class is serializable."""
-        return getattr(thing, "_defaults", None) != None
-
-    def _serialize(self, obj):
-        """Serialize one thing."""
-        if self._is_sclass(obj.__class__):
-            return self._from_pool(obj)
-        if self._is_primitive(obj): return obj
-        if type(obj) is bytes:
-            return {"type": 'bytes', "data": obj.hex()}
-        if type(obj) in Serializer.ITERABLE_TYPES:
-            return {"type": type(obj).__name__, "data":
-                self._serialize_iterable(obj)}
-        if type(obj) is dict:
-            return {"type": "dict", "data":
-                [{"key": self._serialize(k), "value": self._serialize(v)}
-                    for (k, v) in obj.items()]}
-        raise TypeError("Attempting to _serialize non-serializable thing {}".format(obj))
+        """Make sure a thing is serializable."""
+        return getattr(thing.__class__, "_defaults", None) != None
 
     def _from_pool(self, obj):
         """Register an object in the pool."""
@@ -105,7 +155,7 @@ class Serializer:
                 self.pool[obj._uuid] = obj
                 if self.pool_queue:
                     self.pool_queue.append(obj._uuid)
-        return config.get("ref_prefix") + obj._uuid
+        return self.__class__.RPF + obj._uuid
     
     def _pool_iterator(self):
         """Return an iterator over the pool"""
@@ -119,9 +169,6 @@ class Serializer:
             yield (key, val)
             i += 1
     
-    def _serialize_iterable(self, iterable):
-        """Serialize an iterable object"""
-        return [self._serialize(i) for i in iterable]
     def _serialize_class(self, obj, module):
         """Serialize a class object"""
         data = dict()
@@ -136,39 +183,24 @@ class Serializer:
         for key in cls._defaults:
             data[key] = self._serialize(getattr(obj, key))
         return value
-    
-    def _deserialize(self, obj):
-        """Deserialize one object."""
-        rpf = config.get("ref_prefix")
-        if type(obj) is str and obj.startswith(rpf):
-            return self._deserialize_class(self.antipool[obj[len(rpf):]])
-        if self._is_primitive(obj):
-            return obj
-        if type(obj) is dict:
-            dtype = __builtins__.get(obj["type"], None)
-            if dtype in Serializer.ITERABLE_TYPES:
-                return self._deserialize_iterable(obj["data"], dtype)
-            if obj["type"] == "bytes":
-                return bytes.fromhex(obj["data"])
-            if obj["type"] == "dict":
-                return {x["key"]: self._deserialize(x["value"]) for x in obj["data"]}
-        raise TypeError("Unsupported deserialization for {}!".format(obj))
-    
+
+    def _is_dsclass(self, obj):
+        return type(obj) is str and obj.startswith(self.__class__.RPF)
+
     def _lookup_class(self, module, name):
         """Turn a module and a name into a class object"""
         return getattr(import_module(module), name)
 
-    def _deserialize_iterable(self, obj, dtype):
-        return dtype(self._deserialize(i) for i in obj)
-    def _deserialize_class(self, data):
+    def _deserialize_class(self, ref):
         """Deserialize a class object"""
+        data = self.antipool[ref[len(self.__class__.RPF):]]
         uuid = data["data"]["_uuid"]
         if uuid in self.antiset:
             return self.pool[uuid]
         cls = self._lookup_class(data["module"], data["type"])
-        assert self._is_sclass(cls)
         obj = Dummy()
         obj.__class__ = cls
+        assert self._is_sclass(obj)
         # obj = cls()  # Work on signature
         fields = data["data"]
         # obj.__dict__.update({k: self._deserialize(v) for k, v in fields.items()})
@@ -180,7 +212,7 @@ class Serializer:
         self.antiset.add(uuid)
         return obj
 
-class BoundSerializer(Serializer):
+class BoundSerializer(ClassSerializer):
     def __init__(self, name):
         super().__init__()
         path = "{}/{}".format(config.get("dir"), name)
