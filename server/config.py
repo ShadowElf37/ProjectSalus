80b46720e0d9efc868c8a5933a295606ba3a609b
Alwinfy <20421383+Alwinfy@users.noreply.github.com>
2019-06-10 23:12:32 -0500

    Clean up config a little

--- server/config.py
+++ server/config.py
@@ -1,13 +1,9 @@
 import json
-import os
+from os.path    import isdir
+from env        import EnvReader
 
-CONFIG_DIR = '../config'
-if not os.path.isdir(CONFIG_DIR):
-    CONFIG_DIR = './config'
-    assert os.path.isdir(CONFIG_DIR), '__main__ is in a bad location - move it to Salus or Salus/server and try again.'
-
-FMT_STR = '%s/{}.json' % CONFIG_DIR
-
+DEFAULT_LOCATIONS = ('../config', 'config')
+FMT_STR = '{}/{}.json'
 
 class ConfigCache:
     def __init__(self):
@@ -21,22 +17,29 @@ CONFIG_CACHE = ConfigCache()
 
 import os.path
 class Config:
-    def __init__(self, name: str):
+    def __init__(self, name: str, location: str=None):
+        try: conf_dir = next(filter(ispath,
+                DEFAULT_LOCATIONS if location is None else location))
+        except StopIteration:
+            raise ValueError("Can't find suitable config directory!")
         self.fh = open(FMT_STR.format(name), 'r+')
         self.name = name
-        self.data = json.load(self.fh)
-        if not isinstance(self.data, dict):
-            raise ValueError("Top level JSON should always be a dictionary!")
+        self.reload()
 
     def get(self, key):
         return self.data.get(str(key), None)
 
     def reload(self):
-        self.__init__(self.name)
+        self.data = json.load(self.fh)
+        if not isinstance(self.data, dict):
+            raise ValueError("Top level JSON should always be a dictionary!")
 
     # To whom it may concern: self.fh is opened with r+. This means append. Call seek(0) and truncate() before dumping data. Thanks. Also, these configs are supposed to be read-only rn. Dynamic config editing only leads to pain. Thanks again, Alwinfy
     #def __del__(self):
         #self.fh.close()
+    @staticmethod
+    def verify_dir(directory: str) -> bool:
+        return ispath(directory)
 
 
 def get_config(name):
