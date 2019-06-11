bae86ab4442506fb07c70281a4e3b9eadd0986da
ShadowElf37 <ykey-cohen@emeryweiner.org>
2019-06-11 18:02:57 -0500

    ALPHA v0.4.8a (added todo and polls to proprietary.py)

--- proprietary.py
+++ proprietary.py
@@ -21,14 +21,67 @@ class Event:
         self.date = date
         self.time = time
 
+@MiscSerializer.serialized()
+class TodoItem:
+    STATES = [
+        'Pending Approval',
+        'Approved',
+        'Assigned to Representative',
+        'Complete',
+    ]
+    def __init__(self, title):
+        self.title = title
+        self.state = 0
+        self.complete = False
+        self.assigned = None
 
-EVENTS = {}
-CLUBS = {}
+    def assign_to(self, acnt):
+        self.assigned = acnt
+    def finish(self):
+        self.complete = True
+    def advance(self):
+        self.state += 1
+    def get_state(self):
+        return TodoItem.STATES[self.state]
+
+
+@MiscSerializer.serialized()
+class Poll:
+    def __init__(self, title, desc):
+        self.title = title
+        self.desc = desc
+        self.questions = []
+        self.responses = {}
+
+    def get_responses(self, qnum):
+        return self.responses[self.questions[qnum][0]]
+    def get_responses_bytext(self, qtext):
+        return self.responses[qtext]
+
+    def add_response(self, qnum, choice, user=None):
+        question = self.questions[qnum][0]
+        self.responses[question].append((user, choice))
+
+    def add_question(self, qtext, *choices, freeresponse=False, number=None):
+        if number is None: number = len(self.questions)
+        self.questions.insert(number, (qtext, (choices if not freeresponse else None)))
+        self.responses[qtext] = []
+
+    def remove_question(self, idx):
+        self.questions.pop(idx)
+
+EVENTS = {}  # date:Event
+CLUBS = {}  # name:Club
+POLLS = {}  # name:Poll
+TODOLIST = []
 try:
     MiscSerializer.load()
     EVENTS = MiscSerializer.get('EVENTS')
     CLUBS = MiscSerializer.get('CLUBS')
-except (JSONDecodeError, KeyError):
-    pass
-MiscSerializer.set('EVENTS', EVENTS)
-MiscSerializer.set('CLUBS', CLUBS)
+    POLLS = MiscSerializer.get('POLLS')
+    TODOLIST = MiscSerializer.get('TODO')
+finally:
+    MiscSerializer.set('EVENTS', EVENTS)
+    MiscSerializer.set('CLUBS', CLUBS)
+    MiscSerializer.set('POLLS', POLLS)
+    MiscSerializer.set('TODO', TODOLIST)
